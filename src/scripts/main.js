401ed43660f873d726e5bbfec66f17254448b21a
isaacgh0 <garciaisaac468@gmail.com>
2024-07-09 23:06:56 -0600

    docs: 📚 add comments to main js function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,24 @@
 const componentloader = async (filePath) => {
   return new Promise((resolve, reject) => {
+    /* Fetch API: https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
+     * use fetch to load file in same domain
+     */
     fetch(filePath, {
       method: 'GET',
       headers: {
         'Content-Type': 'text/html'
       }
     })
-      .then(response => response.text())
+      .then(response => response.text()) // Transform the data into text
       .then(content => resolve(content))
       .catch(error => reject(error))
   });
 }
 
+/*
+ * Load components and render them in the root element,
+ * use promises all to load all components and render them
+ */
 const components = [
   '/components/header/header.html',
 ]
